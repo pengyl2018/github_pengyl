509b2b71a1fea4611bc1c5eac74c1808ff7fcb5a
pengyl-asus <945523239@qq.com>
2019-07-12 23:25:28 +0800

    update-seventh

--- swordToOffer/src/seventh_binaryTree/TraverseBTree.java
+++ swordToOffer/src/seventh_binaryTree/TraverseBTree.java
@@ -1,5 +1,8 @@
 package seventh_binaryTree;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -60,6 +63,48 @@ public class TraverseBTree {
             }
         }
     }
+    // 后序遍历-非递归
+    /*
+    思路：对于一个节点而言，要实现访问顺序为左儿子-右儿子-根节点，可以利用后进先出的栈，
+    在节点不为空的前提下，依次将根节点，右儿子，左儿子压栈。故我们需要按照根节点-右儿子-左
+    儿子的顺序遍历树，而我们已经知道先序遍历的顺序是根节点-左儿子-右儿子，故只需将先序遍历
+    的左右调换并把访问方式打印改为压入另一个栈即可。最后一起打印栈中的元素。
+     */
+    public static void afterTraverse2(Node root) {
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
+        while(root != null || !stack1.isEmpty()) {
+            while (root != null) {
+                stack1.push(root);
+                stack2.push(root);
+                root = root.right;
+            }
+            if (!stack1.isEmpty()) {
+                root = stack1.pop();
+                root = root.left;
+            }
+        }
+        while (!stack2.isEmpty()) {
+            Node n = stack2.pop();
+            System.out.print(n.data + " ");
+        }
+    }
+
+    // 层次遍历
+    public static void levelTraverse(Node root) {
+        Deque<Node> deque = new LinkedList<>();
+        deque.offer(root);
+        while (!deque.isEmpty()) {
+            Node node = deque.poll();
+            System.out.print(node.data + " ");
+            if (node.left != null) {
+                deque.offer(node.left);
+            }
+            if (node.right != null) {
+                deque.offer(node.right);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Node n1 = new Node(1);
@@ -84,6 +129,10 @@ public class TraverseBTree {
         preTraverse2(n1);
         System.out.println();
         midTraverse2(n1);
+        System.out.println();
+        afterTraverse2(n1);
+        System.out.println();
+        levelTraverse(n1);
     }
 }
 
