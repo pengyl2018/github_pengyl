fea066071ede221e32a7a190e5c158e0cf407330
pengyl-asus <945523239@qq.com>
2020-04-23 22:03:14 +0800

    新增算法：将链表反序

---
+++ algorithm/src/ByteRun/ReverseLinkedList.java
@@ -0,0 +1,42 @@
+package ByteRun;
+
+import common.CommonUtil;
+import common.ListNode;
+
+/**
+ * Reverse a singly linked list.
+ *
+ * Example:
+ *
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * Follow up:
+ *
+ * A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        ListNode newHead = reverseLink(n1);
+        CommonUtil.printLink(newHead);
+    }
+
+    private static ListNode reverseLink(ListNode head) {
+        ListNode pre = null;
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = pre;
+            pre = head;
+            head = tmp;
+        }
+        return pre;
+    }
+}
