f86b47c55be7e02f133b3630042916b57d05a41f
pengyl-asus <945523239@qq.com>
2020-07-26 12:30:32 +0800

    重新导入desighPattern

--- springboot/demo/src/main/java/com/example/demo/Hello.java
+++ springboot/demo/src/main/java/com/example/demo/Hello.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
+    @Autowired
+    TestService testService;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(){
         return "hello springboot";
+    }
+
+    @RequestMapping(value = "/feboo/{number}", method = RequestMethod.GET)
+    public int computeFebonacci(@PathVariable("number") String number) {
+        Integer num = Integer.valueOf(number);
+        return testService.getFebonacci(num);
     }
 }
