1cdc91ea3cd0bc8e7dade0cbc8b7c102d9362ce6
pengyl <peng.yunlei1@zte.com.cn>
2020-08-06 20:08:41 +0800

    动态规划之零钱兑换问题

---
+++ jiketime/src/main/java/dynamicprogramming/CoinsChange.java
@@ -0,0 +1,46 @@
+package dynamicprogramming;
+
+/**
+ * 零钱兑换问题
+ * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * 示例 1:
+ * 输入: coins = [1, 2, 5], amount = 11
+ * 输出: 3
+ * 解释: 11 = 5 + 5 + 1
+ * 
+ * 示例 2:
+ * 输入: coins = [2], amount = 3
+ * 输出: -1
+ * 说明:
+ * 你可以认为每种硬币的数量是无限的。
+ */
+public class CoinsChange {
+    public static void main(String[] args) {
+        CoinsChange coninsChange = new CoinsChange();
+        System.out.println(coninsChange.coinChange(new int[] {1, 2, 5}, 11));
+        System.out.println(coninsChange.coinChange(new int[] {2, 2, 5}, 3));
+    }
+
+    /**
+     * dp状态定义：dp[i]表示总金额为i时，硬币最少的个数 dp方程: if (i > coins[j]) : dp[i] = min(dp[i - coins[j]]) + 1
+     * 
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < coins.length; j++) {
+                if (i >= coins[j] && dp[i - coins[j]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
